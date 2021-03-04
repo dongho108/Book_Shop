4097f29a6f176898ac7c6111dd25cbdff2e512ed
dongho <dongho108@naver.com>
2021-03-04 14:37:42 +0900

    init test lombok, Jnuit4, p6spy ...

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +18,15 @@ public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+
     @Test
     @Transactional
+    @Rollback(false)
     public void testMember() throws Exception {
         //given
         Member member = new Member();
         member.setUsername("memberA");
+
 
         //when
         Long saveId = memberRepository.save(member);
@@ -32,5 +35,8 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("(findMember == member = " + (findMember == member));
     }
+
 }
